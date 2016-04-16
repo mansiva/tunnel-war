9b3379b13f6973abccd5194d94eda624acea1b4e
Ivano Mansueto <imansueto@hibernum.com>
2016-04-16 09:27:06 -0400

    Initial setup

---
+++ Assets/Game/Scripts/Board.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Board : MonoBehaviour
+{
+	[SerializeField] int width;
+	[SerializeField] int height;
+
+	Tile[,] _tiles;
+
+	void Start ()
+	{
+		_tiles = new Tile[width, height];
+
+		Transform tunnelPrefab = transform.GetChild(0);
+		Transform tilePrefab = tunnelPrefab.GetChild(0);
+
+		_tiles[0
+		for (int i = 1; i < height; ++i)
+		{
+			GameObject tile = Instantiate<GameObject>(tilePrefab.gameObject);
+			tile.transform.SetParent(tunnelPrefab);
+		}
+
+		for (int i = 1; i < width; ++i)
+		{
+			GameObject tunnel = Instantiate<GameObject>(tunnelPrefab.gameObject);
+			tunnel.transform.SetParent(transform);
+		}
+	}
+}
